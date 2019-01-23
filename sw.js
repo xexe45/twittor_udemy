c01eedf073daa306577d762e4d292ccf012c2f67
Cesar Lachira <lachiracesar@gmail.com>
2019-01-22 22:36:20 -0500

    Modificaciones de Iphone x

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@
 
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v9';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v10';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -54,6 +54,10 @@ self.addEventListener('activate', e => {
         .then(keys => {
             keys.forEach(key => {
                 if (key != STATIC_CACHE && key.includes('static')) {
+                    return caches.delete(key);
+                }
+
+                if (key != DYNAMIC_CACHE && key.includes('dynamic')) {
                     return caches.delete(key);
                 }
             });
